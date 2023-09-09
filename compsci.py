b94be7664980644216062cd6efd764185bea1f5c
Javi1704 <144072213+Javi1704@users.noreply.github.com>
2023-09-08 19:20:39 -0600

    Add files via upload

---
+++ compsci.py
@@ -0,0 +1,35 @@
+#1
+a = int(input())
+b = int(input())
+
+print(a==(b*20))
+
+#2
+c = int(input())
+d = int(input())
+e = int(input())
+
+print((c*d*e)/(c+d+e))
+
+#3
+f = int(input())
+g = int(input())
+h = int(input())
+
+print(f<g<h)
+
+#4
+i = int(input())
+j = int(input())
+k = int(input())
+l = int(input())
+
+print((a and b and c and d)<=0)
+
+#5
+m = int(input())
+
+if m % 2 == 0:
+        print(True)
+else:
+        print(False)
